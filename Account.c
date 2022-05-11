bebbedd540661eca1cae92e4414a675a210271c4
M.M.Siddik <35638784+mohameddeveloper98@users.noreply.github.com>
2022-05-11 11:07:00 +0200

    adding linked list v1

--- Account.c
+++ Account.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
-
+#include <stdbool.h>
 
 typedef struct
 {
@@ -20,6 +20,11 @@ typedef struct
     int password;
 }Bank_Account;
 
+struct node
+{
+  Bank_Account *Acc;
+  struct node *next;
+};
 
 // Setters:
 void set_Fname(Bank_Account* account, char* name);
@@ -50,13 +55,20 @@ short int get_password(Bank_Account* account);
 int returnRandoms(int lower, int upper);
 
 
+//linked list functions:
+
+void display_AllSavedAccountsData(struct node *head);
+
+struct node* get_Head();
+
+struct node* searchAccNode(struct node* head, int BankID);
 
 
 
 
 int main(int argc, char *argv[])
 {
-    
+
 
 
     return 0;
@@ -69,8 +81,9 @@ int returnRandoms(int lower, int upper)
 }
 
 Bank_Account* CreateAcc(){
+    
     Bank_Account * acc = (Bank_Account*) malloc(sizeof(Bank_Account));
-    acc->bankAccID = returnRandoms(1000 ,9999);
+    acc->bankAccID = returnRandoms(10000 ,99999);
     acc->password = returnRandoms(1000 ,9999);
     acc->accoutStatus = 'A';
     return acc;
@@ -175,4 +188,50 @@ short int get_password(Bank_Account* account){
     
     return account -> password;
 }
+
+void display_AllSavedAccountsData(struct node *head){
+    struct node *temp = head;
+    printf("\n\nList elements are - \n");
+    printf("\nFull_Name,Full_Address,National_ID,Age,Bank_Account_ID,Guardian,Guardian_National_ID,Account_Status,Balance,Password\n");
+    while(temp != NULL) {
+    // printf("\nAccount Fullname: %s --->\n",temp->Acc->fName);
+    // printf("Account ID %d --->\n",temp->Acc->bankAccID);
+    // printf("-------------------\n");
+    printf("%s,%s,%d,%d,%d,%s,%d,%c,%d,%d\n",temp->Acc->fName,temp->Acc->address,temp->Acc->NID,temp->Acc->age,temp->Acc->bankAccID,temp->Acc->guardianName,temp->Acc->guardianNID,temp->Acc->accoutStatus,temp->Acc->balance,temp->Acc->password);
+    temp = temp->next;
+}
+    printf("\n");
+}
+
+struct node* get_Head()
+{
+    int a;
+}
+
+void storeNewAcc (struct node *head, Bank_Account* account)
+{
+    //stroring at the begenning of the linked list
+    struct node *newNode;
+    // newNode = malloc(sizeof(struct node));
+    newNode->Acc = account;
+    newNode->next = head;
+    head = newNode;
+}
+
+/// Search for an Account node ..
+
+struct node* searchAccNode(struct node* head, int BankID) 
+{
+    struct node* current = head;
+    while (current != NULL) {
+        if (current->Acc->bankAccID == BankID) return current;
+        current = current->next;
+    }
+    if (current == NULL){
+        printf("Bank account ID does not exist");
+        return current;
+    }   
+}
+
+
 
